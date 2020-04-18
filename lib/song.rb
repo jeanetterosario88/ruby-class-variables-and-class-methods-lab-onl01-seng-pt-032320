01ac351b12414968366f5ed6c2767fe430de3026
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-18 00:23:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
     def self.genre_count
       genre_count = {}
       @@genres.each do |genre|
-        if !genre_count[genre]
+        if genre_count[genre] != nil
           genre_count[genre] = 1 
         else
           genre_count[genre] += 1
@@ -40,12 +40,12 @@ class Song
     end
     
     def self.artist_count
-      genre_count = {}
-      @@genres.each do |genre|
-        if !genre_count[genre]
-          genre_count[genre] = 1 
+      artist_count = {}
+      @@artists.each do |artist|
+        if !artist_count[artist|
+          artist_count[artist] = 1 
         else
-          genre_count[genre] += 1
+          artist_count[genre] += 1
         end
       end
       genre_count
